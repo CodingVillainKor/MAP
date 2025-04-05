888395d421650e5c8c74002ec1959403fd19faa8
hrim <commit.im@gmail.com>
2025-04-06 00:18:07 +0900

    add load json configuration, ready to **kwargs

--- train.py
+++ train.py
@@ -39,7 +39,8 @@ class Trainer:
 
 
 def main():
-    config = ... # change this by loading from config file
+    with open("config.json", "r") as f:
+        config = json.load(f)
     trainer = Trainer(config)
     trainer.train(5)
     trainer.test()
